64e711baddcce82f15865367fbf0bcb8a20d6399
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-28 21:01:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,6 +20,8 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    "#{params[:number1].to_i}#{params[:operation]}#{params[:number2].to_i}"
+    case params[:operation]
+    when "add"
+      "#{params[:number1]+params[:number2]}"
   end
 end
