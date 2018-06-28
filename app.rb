58bde0aff50e92dc92333813feb9f787fde978ed
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-28 20:23:45 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,6 +11,6 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    "#{params[:number].to_i.times do params[:phrase] end}"
+    "#{params[:number].to_i.times do puts params[:phrase] end}"
   end
 end
